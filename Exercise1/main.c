a7d1a27305a57526aabfdc20eabd38b113b985b4
emanoli14 <148051240+emanoli14@users.noreply.github.com>
2023-10-22 22:18:38 +0100

    Update main.c
    Printing the values of proximity sensors

--- Exercise1/main.c
+++ Exercise1/main.c
@@ -26,23 +26,45 @@ int main(void)
     mpu_init();
 
 
-
-
     messagebus_init(&bus, &bus_lock, &bus_condvar);
     serial_start();
     proximity_start();
+    calibrate_ir();
 
     /* Infinite loop. */
     while (1) {
-    	//waits 1 second
-        chThdSleepMilliseconds(1000);
-        int prox0 = get_prox(0);
-        int prox7 = get_prox(7);
-        int prox1 = get_prox(1);
-        int prox6 = get_prox(6);
+            // Read the values of all proximity sensors
+            int proxValues[8];
+            for (int i = 0; i < 8; i++) {
+                proxValues[i] = get_prox(i);
+            }
 
 
-    }
+            // Print the values of all proximity sensors
+            char str[100];
+            int str_length = sprintf(str, "Proximity Sensor Values: %d %d %d %d %d %d %d %d\n",
+                                     proxValues[0], proxValues[1], proxValues[2], proxValues[3],
+                                     proxValues[4], proxValues[5], proxValues[6], proxValues[7]);
+            e_send_uart1_char(str, str_length);
+
+
+            // Read the values of all calibrated proximity sensors
+            int calibratedValues[8];
+            for (int i = 0; i < 8; i++) {
+                calibratedValues[i] = get_calibrated_prox(i);
+            }
+
+
+            // Print the values of all calibrated proximity sensors
+            char str2[100];
+            int str_length2 = sprintf(str2, "Calibrated Proximity Sensor Values: %d %d %d %d %d %d %d %d\n",
+                                     calibratedValues[0], calibratedValues[1], calibratedValues[2], calibratedValues[3],
+                                     calibratedValues[4], calibratedValues[5], calibratedValues[6], calibratedValues[7]);
+            e_send_uart1_char(str2, str_length2);
+
+            // Wait for a certain interval (1 second in this case)
+            chThdSleepMilliseconds(1000);
+        }
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
