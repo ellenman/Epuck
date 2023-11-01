5dd7ea3aee68e9e02f6b2d876b9d551efb500dd3
emanoli14 <148051240+emanoli14@users.noreply.github.com>
2023-11-01 08:46:23 +0000

    Update main.c
    Selector code implemented still need to add the final code for each task

--- Exercise1/main.c
+++ Exercise1/main.c
@@ -18,66 +18,14 @@
 
 #include "leds.h"
 #include "spi_comm.h"
+#include "selector.h"
+
 
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
 
-// Define the MAX_SPEED constant with a value of 300
-#define MAX_SPEED 300;
 
-bool objectOnTheRight(){
-  bool detectObjectOnTheRight = false;
-  int prox0 = get_calibrated_prox(0);
-  int prox1 = get_calibrated_prox(1);
-
-  if(prox0 > 200 || prox1 > 200 ){
-    char str[100];
-    int str_length = sprintf(str, "Object on the Right  ---- Sensor0: %d, Sensor1: %d\n",
-                                         prox0, prox1);
-    e_send_uart1_char(str, str_length);
-    detectObjectOnTheRight = true;
-
-  }
-  return detectObjectOnTheRight;
-
-}
-
-bool objectOnTheLeft(){
-  bool detectObjectOnTheLeft = false;
-  int prox6 = get_calibrated_prox(6);
-  int prox7 = get_calibrated_prox(7);
-
-
-
-  if(prox6 > 200 || prox7 > 200 ){
-    detectObjectOnTheLeft = true;
-    char str1[100];
-    int str_length1 = sprintf(str1, "Object on the Left  ---- Sensor6: %d, Sensor7: %d\n",
-                                         prox6, prox7);
-    e_send_uart1_char(str1, str_length1);
-  }
-
-  return detectObjectOnTheLeft;
-
-}
-
-bool trapped(){
-  bool robotTrapped = false;
-  int prox6 = get_calibrated_prox(6);
-  int prox1 = get_calibrated_prox(1);
-
-  if(prox1 > 200 && prox6 > 200 ){
-    robotTrapped = true;
-    char str3[100];
-    int str_length3 = sprintf(str3, "Robot is Stuck  ---- Sensor1: %d, Sensor6: %d\n",
-                                         prox1, prox6);
-    e_send_uart1_char(str3, str_length3);
-  }
-
-  return robotTrapped;
-
-}
 
 int main(void)
 {
@@ -95,47 +43,14 @@ int main(void)
     clear_leds();
     spi_comm_start();
 
-
     /* Infinite loop. */
     while (1) {
-
-    	set_body_led(1);
-
-      int left_speed = MAX_SPEED;
-      int right_speed = MAX_SPEED;
-      rotating = false;
-
-      if(trapped()){
-        if(counter < 10){
-          left_speed = -MAX_SPEED;
-          right_speed = MAX_SPEED;
-          rotating = true;
-          counter++;
+        if(get_selector() == 1) {
+          //Task1
+        } else if(get_selector() == 2) {
+          //Task2
         }
-        else{
-          left_speed = MAX_SPEED;
-          right_speed = MAX_SPEED;
-          rotating = false;
-          counter = 0;
-        }
-
-      }
-      else if(objectOnTheRight() && !rotating){
-      	set_body_led(0);
-
-		left_speed = -MAX_SPEED;
-        right_speed = MAX_SPEED;
-      }
-      else if(objectOnTheLeft() && !rotating){
-    	set_body_led(0);
-        left_speed = MAX_SPEED;
-        right_speed = -MAX_SPEED;
-      }
-
-      left_motor_set_speed(left_speed);
-      right_motor_set_speed(right_speed);
-    	//waits 1 second
-      chThdSleepMilliseconds(100);
+        chThdSleepMilliseconds(100);
     }
 }
 
